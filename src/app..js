0e01963b34ceb4f3a1964e6e3a48db3605745119
devankitmishra <ankitmishrapuri@gmail.com>
2024-07-28 20:52:42 +0530

    utils code , api res err handling

---
+++ src/app..js
@@ -0,0 +1,19 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit:"16kb"}))//data from form accecting json
+app.use(express.urlencoded({extended:true,limit:"16kb"}))//data from url
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
+
+export {app}
--- src/index.js
+++ src/index.js
@@ -10,6 +10,18 @@ dotenv.config({
 
 
 connectDB()
+.then(() => {
+  app.on("error",(error) =>{//assignment
+    console.log("ERROR:",error);
+    throw error
+  })//assignment
+  app.listen(process.env.PORT || 8000, () =>{
+    console.log(` Server is running at port : ${process.env.PORT}`);
+  })
+})
+.catch((err)=>{
+  console.log("MONGODB Connection Failled !!! ",err);
+})
 
 
 

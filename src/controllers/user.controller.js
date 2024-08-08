6054c7bb7e6996831fcdbc07beaebf4c19d7021a
devankitmishra <ankitmishrapuri@gmail.com>
2024-08-08 22:51:22 +0530

    fix register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -16,7 +16,7 @@ const registerUser = asyncHandler(async (req, res) => {
   // return res
 
   const { fullName, email, username, password } = req.body;
-  console.log("name: ", fullName, "email: ", email);
+  // console.log("name: ", fullName, "email: ", email);
 
   if (
     [fullName, email, username, password].some((field) => 
@@ -26,16 +26,23 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or:[{ username },{ email }]
   })
 
   if(existedUser){
     throw new ApiError(409, "User with email or username already exists");
   }
+  //console.log(req.files);
 
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  let coverImageLocalPath;
+  if (req.files && Array.isArray(req.files.coverImage) 
+    && req.files.coverImage.length > 0) {
+    coverImageLocalPath = req.files.coverImage[0].path
+  }
 
   if (!avatarLocalPath) {
     throw new ApiError(400,"Avatar file is required")
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import connectDB from "./db/index.js";
 import {app} from './app.js'
 
 dotenv.config({
-    path: './env'
+    path: './.env'
 })
 
 
